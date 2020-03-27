e016e2af47dd9cf51730011432c2465fc141c332
devonkhosa <dalvinderkhosa@gmail.com>
2020-03-27 13:19:00 -0700

    Moved to v2.0.0

--- Readme.md
+++ Readme.md
@@ -3,3 +3,7 @@ Within this folder lies a simple e-commerce platform using node.js, express, and
 This was a test!
 
 ;^)
+
+v1.0.0 - Old, broken. moved on to v.2.0.0
+
+v2.0.0 - current
--- v.1.0.0/shopping-cart/index.js
+++ v.1.0.0/shopping-cart/index.js
@@ -3,14 +3,12 @@ const app = express();
 const port = 3000;
 
 //Middlewares
-app.set('view engine', 'pug')
-
+app.set('view engine', 'pug');
+app.use(express.static('public'));
 //Modules
-const entry_point = require('./entry-point')
+const entry_point = require('./routes/entry-point')
 
-app.get('/', (req, res)=> {
-    res.render('Hello world!')
-});
+app.get('/', entry_point)
 
 app.listen(port, ()=> {
     console.log(`Running on port: ${port}`)
